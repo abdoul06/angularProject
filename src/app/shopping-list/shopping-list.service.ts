2e3419e55f2947ac8c23e4fb0ff1c8d3084ada9e
abdoul06 <82101190+abdoul06@users.noreply.github.com>
2021-07-07 15:54:33 -0300

    observables added to the course

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable()
 export class ShoppingListService {
-  ingredientChanged = new EventEmitter<Ingredient[]>()
+  ingredientChanged = new Subject<Ingredient[]>()
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -18,7 +20,7 @@ export class ShoppingListService {
 
   onIngredientAdded(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
-    this.ingredientChanged.emit(this.ingredients.slice())
+    this.ingredientChanged.next(this.ingredients.slice())
   }
 
   addIngredients(ingredients: Ingredient[]) {
@@ -26,7 +28,7 @@ export class ShoppingListService {
     //   this.addIngredients(ingredient);
     // }
     this.ingredients.push(...ingredients);
-    this.ingredientChanged.emit(this.ingredients.slice())
+    this.ingredientChanged.next(this.ingredients.slice())
   }
 
 }
